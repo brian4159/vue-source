bc1cfe00ce36d1ec21e39c792c818c46c55aacbe
王威 <ww322030.jx@chinatelecom.cn>
2024-06-05 23:20:58 +0800

    深度属性劫持

--- src/observe/index.js
+++ src/observe/index.js
@@ -4,14 +4,33 @@ export function observe(data){
      return;
    }
 
-
    return new Observer(data)
 }
 
 
 class Observer{
     constructor(data){
-        
+          this.walk(data)
     }
 
+    walk(data){
+      //重新定义属性
+      Object.keys(data).forEach(key=>defineReactive(data,key,data[key]))
+    }
+
+}
+
+export function defineReactive(target,key,value){
+  observe(value) //对所有的对象进行属性劫持
+  Object.defineProperty(target,key,{
+    get(){
+      console.log('取值');
+      return value
+    },
+    set(newValue){
+      console.log('设置值');
+      if(value === newValue)return
+      value = newValue
+    }
+  })
 }
--- src/state.js
+++ src/state.js
@@ -7,10 +7,27 @@ export  function initState(vm){
     }
 }
 
+function proxy(vm,target,key){
+    Object.defineProperty(vm,key,{
+        get(){
+            return vm[target][key]
+        },
+        set(newValue){
+            vm[target][key]  = newValue
+        }
+    })
+}
+
 function initData(vm){ 
     let data = vm.$options.data
     data = typeof data == 'function'? data.call(vm):data;
-
+    vm._data = data
+    
     observe(data)
+
+     for(let key in data){
+        proxy(vm,'_data',key)
+     }
+    
 }
 
